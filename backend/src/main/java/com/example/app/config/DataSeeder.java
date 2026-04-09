e50dcfbda90c41f6251eda555e864684461ca328
David Mirc <dmirc@hosp-it.eu>
2026-04-09 09:11:00 +0200

    Modules

--- backend/src/main/java/com/example/app/config/DataSeeder.java
+++ backend/src/main/java/com/example/app/config/DataSeeder.java
@@ -54,6 +54,7 @@ public class DataSeeder implements CommandLineRunner {
         seedTenant("CoreGym", "jdukaric@calendra.si");
         seedTenant("Nejc Bracko s.p.", "nbracko@calendra.si");
         seedTenant("Inštitut AVISENSA", "nina@avisensa.com");
+        seedTenant("Rudi Inc.", "astraus81@gmail.com");
         seedTenant("Zoom Marketplace", "marketplace@zoom.us");
         seedTenant("Facebook", "developers@facebook.com");
     }
