042655bffd7f9bf2df685a472b4dcd5f30aad281
David Mirc <dmirc@hosp-it.eu>
2026-04-22 07:07:27 +0200

    Inbox

--- guest-mobile/iosApp/GuestMobile/AppEnvironment.swift
+++ guest-mobile/iosApp/GuestMobile/AppEnvironment.swift
@@ -3,7 +3,7 @@ import Foundation
 final class AppEnvironment {
     static let shared = AppEnvironment()
 
-    let baseURL: URL = URL(string: "http://192.168.1.88:4000")!
+    let baseURL: URL = URL(string: "http://10.0.2.2:4000")!
     let usePreviewData: Bool = false
     let googleClientId: String = "YOUR_GOOGLE_IOS_CLIENT_ID"
 
