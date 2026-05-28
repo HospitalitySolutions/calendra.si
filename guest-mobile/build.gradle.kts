776589f488e9d63a5392f065317e0c0cdf5a2e48
David Mirc <dmirc@hosp-it.eu>
2026-05-28 07:19:50 +0200

    Modules

--- guest-mobile/build.gradle.kts
+++ guest-mobile/build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "9.1.1" apply false
-    id("com.android.kotlin.multiplatform.library") version "9.1.1" apply false
+    id("com.android.application") version "9.2.1" apply false
+    id("com.android.kotlin.multiplatform.library") version "9.2.1" apply false
     kotlin("multiplatform") version "2.3.20" apply false
     kotlin("plugin.serialization") version "2.3.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
