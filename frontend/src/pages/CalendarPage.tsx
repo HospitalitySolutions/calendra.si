3d487bf0075678088d87e8a1320e75516a026378
David Mirc <dmirc@hosp-it.eu>
2026-08-13 14:58:43 +0200

    consumables

--- frontend/src/pages/CalendarPage.tsx
+++ frontend/src/pages/CalendarPage.tsx
@@ -1,3 +1,5 @@
+import '../styles/features/clients-and-detail.css'
+import '../styles/features/modern-clients.css'
 import '../styles/calendar/calendar-shell.css'
 import '../styles/calendar/calendar-events.css'
 import '../styles/calendar/calendar-responsive.css'
