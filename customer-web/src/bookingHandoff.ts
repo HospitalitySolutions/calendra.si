15e7e3243d4e85757c92b8ea03d0072739e72562
David Mirc <dmirc@hosp-it.eu>
2026-08-21 11:14:45 +0200

    test

--- customer-web/src/bookingHandoff.ts
+++ customer-web/src/bookingHandoff.ts
@@ -3,7 +3,10 @@ import { MARKETING_BASE_URL } from './config'
 
 export async function launchCustomerBooking(locationId: string | number, sessionTypeId?: string | number | null) {
   const handoff = await customerApi.bookingHandoff(locationId, sessionTypeId)
-  const url = new URL(handoff.bookingUrl, MARKETING_BASE_URL)
+  const bookingBaseUrl = window.location.hostname === 'staging.calendra.si'
+    ? window.location.origin
+    : MARKETING_BASE_URL
+  const url = new URL(handoff.bookingUrl, bookingBaseUrl)
   url.hash = new URLSearchParams({ customerHandoff: handoff.handoffToken }).toString()
   window.location.assign(url.toString())
 }
